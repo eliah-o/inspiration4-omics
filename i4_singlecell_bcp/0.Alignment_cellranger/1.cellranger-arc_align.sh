b5deec689b758c9ec6fb7433004879da2624050e
JangKeun Kim <63000193+jang1563@users.noreply.github.com>
2024-06-11 16:50:50 -0400

    Add files via upload

---
+++ i4_singlecell_bcp/0.Alignment_cellranger/1.cellranger-arc_align.sh
@@ -0,0 +1,5 @@
+cellranger-arc count --id=SAMPLE_NAME \
+                       --reference=path_to_reference_genome \
+                       --libraries=path_to_GEX_and_ATAC \
+                       --localcores=16 \
+                       --localmem=512
---
+++ i4_singlecell_bcp/0.Alignment_cellranger/2.cellranger-vdj.sh
@@ -0,0 +1,5 @@
+cellranger vdj --id=SAMPLE_NAME \
+                 --reference=path_to_reference \
+                 --fastqs=path_to_fastqs \
+                 --localcores=24 \
+                 --localmem=128
